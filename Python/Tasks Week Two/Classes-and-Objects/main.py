efc42cbfaa3755f99e55439ea237b2f5aab31818
musawakiliML <musawakiliml@hotmail.com>
2022-05-28 03:23:53 +0100

    Updated the Class with methods to change name, age, tracks and get score

--- Python/Tasks Week Two/Classes-and-Objects/main.py
+++ Python/Tasks Week Two/Classes-and-Objects/main.py
@@ -1,5 +1,5 @@
 class Student:
-    # [assignment] Skeleton class. Add your code here
+
     def __init__(self, name, age, tracks, score):
         self.name = name
         self.age = age
@@ -7,22 +7,33 @@ class Student:
         self.score = score
 
     def change_name(self, new_name):
-        pass
+        self.name = new_name
 
-    def change_age(self, new_name):
-        pass
+    def change_age(self, new_age):
+        self.age = int(new_age)
 
     def add_track(self, new_track):
-        pass
+        self.tracks.append(new_track)
 
     def get_score(self):
-        pass
+        return self.score
 
 
 Bob = Student(name="Bob", age=26, tracks=["FE", "BE"], score=20.90)
 
+print(Bob.name)
+print(Bob.age)
+print(Bob.tracks)
+print(Bob.get_score())
+
 # Expected methods
+print("Changed the values")
 Bob.change_name("Peter")
 Bob.change_age(34)
 Bob.add_track("UI/UX")
 Bob.get_score()
+
+print(Bob.name)
+print(Bob.age)
+print(Bob.tracks)
+print(Bob.get_score())
