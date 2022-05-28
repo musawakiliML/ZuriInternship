5a09faa50e389565a70e5c70b3a6735a6d9268f2
musawakiliML <musawakiliml@hotmail.com>
2022-05-28 03:05:24 +0100

    Created empty initialization methods

--- Python/Tasks Week Two/Classes-and-Objects/main.py
+++ Python/Tasks Week Two/Classes-and-Objects/main.py
@@ -1,11 +1,25 @@
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self):
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
+
+    def change_name(self, new_name):
+        pass
+
+    def change_age(self, new_name):
+        pass
+
+    def add_track(self, new_track):
+        pass
+
+    def get_score(self):
         pass
 
 
-
-Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+Bob = Student(name="Bob", age=26, tracks=["FE", "BE"], score=20.90)
 
 # Expected methods
 Bob.change_name("Peter")
