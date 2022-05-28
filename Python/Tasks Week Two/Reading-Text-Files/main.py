342873c0a963994e9ba5957eaabfba5c105cf0fc
musawakiliML <musawakiliml@hotmail.com>
2022-05-28 02:40:43 +0100

    Modified the main function to read file and count words in a file

--- Python/Tasks Week Two/Reading-Text-Files/main.py
+++ Python/Tasks Week Two/Reading-Text-Files/main.py
@@ -1,16 +1,33 @@
 # Read text from a file, and count the occurence of words in that text
 # Example:
-# count_words("The cake is done. It is a big cake!") 
+# count_words("The cake is done. It is a big cake!")
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+from collections import Counter
+
 
 def read_file_content(filename):
-    # [assignment] Add your code here 
-    
-    return "Hello World"
+    with open(filename) as file_content:
+        contents = file_content.read()
+    # print(contents)
+    return contents
+
+# read_file_content("story.txt")
 
 
 def count_words():
     text = read_file_content("./story.txt")
-    # [assignment] Add your code here
+    words = text.strip().split()
+    # counting_words = Counter(words)
+    # print(counting_words)
+    counter = dict()
 
-    return {"as": 10, "would": 20}
+    for word in words:
+        if word in counter:
+            counter[word] += 1
+        else:
+            counter[word] = 1
+
+    return counter
+
+
+print(count_words())
