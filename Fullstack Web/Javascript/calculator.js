e8c7dd0ee00fa868037222a6f56a29c2e300bb83
musawakiliML <musawakiliml@hotmail.com>
2022-06-24 00:24:54 +0100

    Adding calculator

--- Fullstack Web/Javascript/calculator.js
+++ Fullstack Web/Javascript/calculator.js
@@ -3,5 +3,25 @@ Build a basic arithmetic calculator without a frontend
 The calculator should be able to perform basic operations like Addition, Subtraction, Multiplication, & Division
 */
 
+// user input, two number and operation
 
-let number = prompt("Enter First Number:")
+let number = Number(prompt("Enter First Number:"))
+
+var operations = prompt("Choose Operation:(+, -, *, /)")
+
+let number_2 = Number(prompt("Enter Second Number:"))
+
+// program logic and expression
+
+if (operations == "+"){
+    window.alert(number + number_2)
+}
+else if(operations == "-"){
+    window.alert(number - number_2)
+}
+else if(operations == "*"){
+    window.alert(number * number_2)
+}
+else if(operations == "/"){
+    window.alert(number / number_2)
+}
