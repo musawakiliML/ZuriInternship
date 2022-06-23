36ac1f8f6634fc353b96a569b5c668a72a9e97fa
musawakiliML <musawakiliml@hotmail.com>
2022-06-24 00:10:19 +0100

    Creating Calculator

---
+++ Fullstack Web/Javascript/calculator.js
@@ -0,0 +1,7 @@
+/*
+Build a basic arithmetic calculator without a frontend
+The calculator should be able to perform basic operations like Addition, Subtraction, Multiplication, & Division
+*/
+
+
+let number = prompt("Enter First Number:")
