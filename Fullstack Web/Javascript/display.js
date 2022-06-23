5654f2a127793ae36c8ef529145bff83c95bb7cd
musawakiliML <musawakiliml@hotmail.com>
2022-06-23 23:51:13 +0100

    Added javascript output

---
+++ Fullstack Web/Javascript/display.js
@@ -0,0 +1,19 @@
+// Write a javascript code that prints out your name, height, 
+// and country on the screen
+
+
+// variable declarations
+let my_name = "Musa Adamu Wakili";
+let height = "1.65 meters";
+let country = "Nigeria";
+
+// displaying output
+console.log(my_name);
+console.log(height);
+console.log(country);
+
+
+// display to windows screen
+window.alert("Name: " + my_name + "\nHeight: " + height + "\nCountry: " + country);
+//window.alert(height);
+//window.alert(country);
